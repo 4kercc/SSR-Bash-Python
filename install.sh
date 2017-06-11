8437edd0f1101d723436c8335d8d3b6a26417700
iPatrick <wmy461737664@qq.com>
2017-06-11 16:55:30 +0800

    修复SSR链接的IP地址错误的问题

--- install.sh
+++ install.sh
@@ -151,6 +151,7 @@ chmod +x /usr/local/bin/ssr
 #Modify ShadowsocksR API
 sed -i "s/sspanelv2/mudbjson/g" /usr/local/shadowsocksr/userapiconfig.py
 sed -i "s/UPDATE_TIME = 60/UPDATE_TIME = 10/g" /usr/local/shadowsocksr/userapiconfig.py
+sed -i "s/SERVER_PUB_ADDR = '127.0.0.1'/SERVER_PUB_ADDR = '$(wget -qO- -t1 -T2 ipinfo.io/ip)'/" /usr/local/shadowsocksr/userapiconfig.py
 #INstall Success
 bash /usr/local/SSR-Bash-Python/self-check.sh
 echo '安装完成！输入 ssr 即可使用本程序~'
